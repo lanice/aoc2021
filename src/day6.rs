423de7ffcfc39093d0ee02fb4af277fbe52347a5
Leander Neiß <1871704+lanice@users.noreply.github.com>
2021-12-06 09:03:10 +0100

    Refactor.

--- src/day6.rs
+++ src/day6.rs
@@ -7,37 +7,25 @@ fn generator_input(input: &str) -> Vec<i32> {
 }
 
 #[aoc(day6, part1)]
-fn part1(input: &[i32]) -> i32 {
-    let mut ages = input.to_vec();
-
-    for _ in 0..80 {
-        let mut new_ages = vec![];
-        for &age in &ages {
-            if age == 0 {
-                new_ages.push(6);
-                new_ages.push(8);
-            } else {
-                new_ages.push(age - 1);
-            }
-        }
-        ages = new_ages;
-    }
-
-    ages.len() as i32
+fn part1(input: &[i32]) -> usize {
+    populate(input, 80)
 }
 
 #[aoc(day6, part2)]
 fn part2(input: &[i32]) -> usize {
+    populate(input, 256)
+}
+
+fn populate(input: &[i32], days: usize) -> usize {
     let mut age_counts = vec![0usize;9];
 
-    for &age in input {
-        age_counts[age as usize] += 1;
+    for &f in input {
+        age_counts[f as usize] += 1;
     }
 
-    for _ in 0..256 {
-        let first = age_counts.drain(0..1).collect::<Vec<_>>()[0];
-        age_counts[6] += first;
-        age_counts.push(first);
+    for _ in 0..days {
+        age_counts.rotate_left(1);
+        age_counts[6] += age_counts[8];
     }
 
     age_counts.iter().sum::<usize>()
